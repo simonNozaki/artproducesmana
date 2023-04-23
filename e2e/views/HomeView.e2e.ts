68efcd1ed2475a3581c4a7786bd4fe6fc044c685
snozaki <snozaki@netprotections.co.jp>
2023-04-23 17:16:50 +0900

    ストーリー追加

--- e2e/views/HomeView.e2e.ts
+++ e2e/views/HomeView.e2e.ts
@@ -1,16 +1,50 @@
 import { test, expect } from "@playwright/test";
 
-test("トップページが開ける", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.locator(".title")).toHaveText("CastAnArt");
+test.describe("画面遷移、表示", () => {
+  test("トップページが開ける", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.locator(".title")).toHaveText("CastAnArt");
+  });
+
+  test("ハンバーガーメニューを開ける", async ({ page }) => {
+    await page.goto("/");
+    await page.locator(".global-navigation-button").click();
+    await expect(page.locator(".navigation-side-bar")).toBeVisible();
+  });
+
+  test("aboutに遷移できる", async ({ page }) => {
+    await page.goto("/");
+    // グローバルメニューからaboutに遷移する
+    await page.locator(".global-navigation-button").click();
+    await page.getByText("CastAnArtとは").click();
+    await expect(page).toHaveURL(/.*\/about/);
+  });
+});
+
+test.describe("カードリスト", () => {
+  test("アコーディオンを開ける", async ({ page }) => {
+    await page.goto("/");
+    await page.locator(".global-navigation-button").click();
+    await page.getByText("収録カード").click();
+    await page.getByText("土地").click();
+    // 土地のリストがアコーディオンで出現する
+    await expect(page.locator(".card-list")).toBeVisible();
+  });
 });
 
 test.describe("トップページ上の操作", () => {
   test("画像を押して拡大できる", async ({ page }) => {
     await page.goto("/");
     // 画像をクリックしてモーダルを開く
-    page.locator("#landimage").click();
+    await page.locator("#landimage").click();
     await expect(page.locator(".dialog-panel")).toBeVisible();
+  });
+
+  test("モーダルを開いて閉じる", async ({ page }) => {
+    await page.goto("/");
+    // 画像をクリックしてモーダルを開く
+    await page.locator("#landimage").click();
+    await page.locator(".modal-close").click();
   });
 
   test("次に進むボタンで画像が切替わる", async ({ page }) => {
@@ -19,7 +53,7 @@ test.describe("トップページ上の操作", () => {
       .locator(".land-image-label")
       .textContent();
     // 次に進むボタンをクリック
-    page.locator("#art-forward-button").click();
+    await page.locator(".art-forward-button").click();
     const nextImageTitle = await page
       .locator(".land-image-label")
       .textContent();
